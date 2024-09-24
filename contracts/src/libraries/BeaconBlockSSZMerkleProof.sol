b5b2bb7ad7edeb6708f67b35ffcdf09fef62dc09
Nicolas Maurice <nmvalera@MacBook-Pro-de-Nicolas.local>
2024-09-24 16:21:51 +0200

    feat(contracts): add SSZ libraries to verify proofs on chain

---
+++ contracts/src/libraries/BeaconBlockSSZMerkleProof.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./SSZMerkleProof.sol";
+
+/// @title BeaconBlockSSZMerkleProof
+/// @notice Library for verifying Beacon Block SSZ Merkle proofs
+library BeaconBlockSSZMerkleProof {
+    uint256 public constant WITHRAWAL_ROOT_GENERALIZED_INDEX = 0x192e;
+
+    /// @notice The depth of the Withdrawals Root in the Beacon Block SSZ Merkle tree
+    uint256 public constant WITHRAWAL_ROOT_GENERALIZED_INDEX_DEPTH = 0xc;
+
+    /// @notice Verifies a Withdrawals Root Merkle proof
+    /// @param _withdrawalsRoot: The Withdrawals Root to verify
+    /// @param _proofs: The Merkle proof
+    /// @param _beaconRoot: The Beacon Root to verify against
+    /// @return True if the proof is valid, false otherwise
+    function verifyWithdrawalsRootProof(bytes32 _withdrawalsRoot, bytes32[] memory _proofs, bytes32 _beaconRoot)
+        external
+        pure
+        returns (bool)
+    {
+        require(_proofs.length == WITHRAWAL_ROOT_GENERALIZED_INDEX_DEPTH, "L2BeaconRootVerifier: Invalid proof length");
+        return SSZMerkleProof._verify(_proofs, _beaconRoot, _withdrawalsRoot, WITHRAWAL_ROOT_GENERALIZED_INDEX);
+    }
+}
---
+++ contracts/test/libraries/SSZMerkleProof.test.sol
@@ -0,0 +1,80 @@
+//SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import "../../src/libraries/SSZMerkleProof.sol";
+
+contract L1BeaconRootsSenderTest is Test {
+    function test_processProofAtIndex_2() public pure {
+        bytes32 root = bytes32(0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91);
+        bytes32 leaf = bytes32(0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561);
+        bytes32[] memory proofs = new bytes32[](1);
+        proofs[0] = bytes32(uint256(0x7c192a721d1eb9102864609e7c7cbede0a368252c3de10ca3a1c778cbd4e9ab1));
+        assertTrue(SSZMerkleProof._verify(proofs, root, leaf, 2));
+    }
+
+    function test_processProofAtIndex_3() public pure {
+        bytes32 root = bytes32(0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91);
+        bytes32 leaf = bytes32(0x7c192a721d1eb9102864609e7c7cbede0a368252c3de10ca3a1c778cbd4e9ab1);
+        bytes32[] memory proofs = new bytes32[](1);
+        proofs[0] = bytes32(uint256(0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561));
+        assertTrue(SSZMerkleProof._verify(proofs, root, leaf, 3));
+    }
+
+    function test_processProofAtIndex_5() public pure {
+        bytes32 root = bytes32(0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91);
+        bytes32 leaf = bytes32(0x141a2946603cbca266b2524b01d156dde2346ff7ff2c1f1568efd53bed1426e8);
+        bytes32[] memory proofs = new bytes32[](2);
+        proofs[0] = bytes32(0x16762883075df957aed6579aba8df69c2ed8d870731dac92b44a4213f8d5fcb5);
+        proofs[1] = bytes32(0x7c192a721d1eb9102864609e7c7cbede0a368252c3de10ca3a1c778cbd4e9ab1);
+
+        // Step1: sha256(0x16762883075df957aed6579aba8df69c2ed8d870731dac92b44a4213f8d5fcb5, 0x141a2946603cbca266b2524b01d156dde2346ff7ff2c1f1568efd53bed1426e8) = 0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561
+        // Step2: sha256(0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561, 0x7c192a721d1eb9102864609e7c7cbede0a368252c3de10ca3a1c778cbd4e9ab1) = 0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91
+        assertTrue(SSZMerkleProof._verify(proofs, root, leaf, 5));
+    }
+
+    function test_processProofAtIndex_6() public pure {
+        bytes32 root = bytes32(0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91);
+        bytes32 leaf = bytes32(0xbc83ba0f33292b2f8b38fdcbce64f6eb75380442c3c802a2a3c16d3a85fad73a);
+        bytes32[] memory proofs = new bytes32[](2);
+        proofs[0] = bytes32(0xf5a5fd42d16a20302798ef6ed309979b43003d2320d9f0e8ea9831a92759fb4b);
+        proofs[1] = bytes32(0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561);
+
+        // Step1: sha256(0xbc83ba0f33292b2f8b38fdcbce64f6eb75380442c3c802a2a3c16d3a85fad73a, 0xf5a5fd42d16a20302798ef6ed309979b43003d2320d9f0e8ea9831a92759fb4b) = 0x7c192a721d1eb9102864609e7c7cbede0a368252c3de10ca3a1c778cbd4e9ab1
+        // Step2: sha256(0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561, 0x7c192a721d1eb9102864609e7c7cbede0a368252c3de10ca3a1c778cbd4e9ab1) = 0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91
+        assertTrue(SSZMerkleProof._verify(proofs, root, leaf, 6));
+    }
+
+    function test_processProofAtIndex_12() public pure {
+        bytes32 root = bytes32(0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91);
+        bytes32 leaf = bytes32(0x2bb8850d73d081d05e5b21e9f6584f10e3f77bf2eecc1dbd63a843f0ccc0a6a8);
+        bytes32[] memory proofs = new bytes32[](3);
+        proofs[0] = bytes32(0x0000000000000000000000000000000000000000000000000000000000000000);
+        proofs[1] = bytes32(0xf5a5fd42d16a20302798ef6ed309979b43003d2320d9f0e8ea9831a92759fb4b);
+        proofs[2] = bytes32(0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561);
+
+        assertTrue(SSZMerkleProof._verify(proofs, root, leaf, 12));
+    }
+
+    function test_processProofAtIndex_6446() public pure {
+        bytes32 root = bytes32(0x6cd96952b3dacfc7188b9ad9b17a2012b67827741f60b3c4e51a14f9d07f0c91);
+        bytes32 leaf = bytes32(0x2e0130c856001a39482aa288f64f001a619dc46a5377b8c278776a41b3a62269);
+
+        bytes32[] memory proofs = new bytes32[](12);
+        proofs[0] = bytes32(uint256(0x0000060000000000000000000000000000000000000000000000000000000000));
+        proofs[1] = bytes32(uint256(0x1c81242091b62f06c68069f0574d78b7d5e2305ec0638d70aea9ddf4842f7cc2));
+        proofs[2] = bytes32(uint256(0xa9a30aed71d87b062cdaf6a68476d1fb88ba17824d33073fc0a47c14ee2f0542));
+        proofs[3] = bytes32(uint256(0xa55982c5098e75240dfa67589b6087b473360058919c1354f54fec07d0d54964));
+        proofs[4] = bytes32(uint256(0x2b075536cd68d87c6a42236945f8946ec654d1603cea26fd63fcfb714bc4e678));
+        proofs[5] = bytes32(uint256(0x13b46e5c802956f74b294cd43fee7f7fbe2786fa5234312142919ac6fba8122d));
+        proofs[6] = bytes32(uint256(0x94e05d13e26c028d1e415400d68a54fe931d72a5fe482dabf79d1c52dd82573a));
+        proofs[7] = bytes32(uint256(0xdb56114e00fdd4c1f85c892bf35ac9a89289aaecb1ebd0a96cde606a748b5d71));
+        proofs[8] = bytes32(uint256(0xb8704d40a68b6bddae5e2d83c5be32959491185c4e41a5e355181845922d000c));
+        proofs[9] = bytes32(uint256(0x0000000000000000000000000000000000000000000000000000000000000000));
+        proofs[10] = bytes32(uint256(0xf5a5fd42d16a20302798ef6ed309979b43003d2320d9f0e8ea9831a92759fb4b));
+        proofs[11] = bytes32(uint256(0xef91c6e7d3789ed96c6659a2641987fdeefee8c3fd6812f74ceeb258a3ce1561));
+
+        assertTrue(SSZMerkleProof._verify(proofs, root, leaf, 6446));
+    }
+}
