76142308a62f8ed3b6cc2f776ee03e36746df66a
Nicolas Maurice <nmvalera@MacBook-Pro-de-Nicolas.local>
2024-09-26 17:34:26 +0200

    feat(contracts/libraries/BeaconBlockSSZMerkleProof): set constant visibility to internal

--- contracts/src/libraries/BeaconBlockSSZMerkleProof.sol
+++ contracts/src/libraries/BeaconBlockSSZMerkleProof.sol
@@ -6,10 +6,10 @@ import "./SSZMerkleProof.sol";
 /// @title BeaconBlockSSZMerkleProof
 /// @notice Library for verifying Beacon Block SSZ Merkle proofs
 library BeaconBlockSSZMerkleProof {
-    uint256 public constant WITHRAWAL_ROOT_GENERALIZED_INDEX = 0x192e;
+    uint256 internal constant WITHRAWAL_ROOT_GENERALIZED_INDEX = 0x192e;
 
     /// @notice The depth of the Withdrawals Root in the Beacon Block SSZ Merkle tree
-    uint256 public constant WITHRAWAL_ROOT_GENERALIZED_INDEX_DEPTH = 0xc;
+    uint256 internal constant WITHRAWAL_ROOT_GENERALIZED_INDEX_DEPTH = 0xc;
 
     /// @notice Verifies a Withdrawals Root Merkle proof
     /// @param _withdrawalsRoot: The Withdrawals Root to verify
