1438ad34db0b8e71a40ba6762fbb09f8cad7b270
Nicolas Maurice <nmvalera@MacBook-Pro-de-Nicolas.local>
2024-09-26 20:34:05 +0200

    feat(contracts/libraries/BeaconRoots): optimize gas

--- contracts/src/libraries/BeaconRoots.sol
+++ contracts/src/libraries/BeaconRoots.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.20;
 /// @title SSZMerkleProof
 /// @notice Library for verifying SSZ Merkle proofs
 library BeaconRoots {
-    function _get(address _beaconRoots, uint256 _timestamp) internal view returns (bytes32 beaconRoot) {
+    function _get(address _beaconRoots, uint256 _timestamp) internal view returns (bytes32) {
         assembly {
             let data := mload(0x40)
             mstore(data, _timestamp)
             pop(staticcall(gas(), _beaconRoots, data, 0x20, data, 0x20))
-            if eq(returndatasize(), 0x20) { beaconRoot := mload(data) }
+            if eq(returndatasize(), 0x20) { return(data, 0x20) }
+            return(0, 0)
         }
     }
 }
