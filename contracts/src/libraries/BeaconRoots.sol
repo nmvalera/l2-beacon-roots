900654dd8a841d0a8773f7957f2ccaa22fb700e8
Nicolas Maurice <nmvalera@MacBook-Pro-de-Nicolas.local>
2024-09-26 18:39:30 +0200

    feat(contracts/libraries/BeaconRoots): reduce _get gas cost by replacing expensive abi.encode/decode with inline assembly
    test_get_whenRootMissing() (gas: -532 (-4.870%))
    test_get_whenRootNotMissing() (gas: -612 (-5.554%))
    Overall gas change: -1144 (-5.213%)

--- contracts/src/libraries/BeaconRoots.sol
+++ contracts/src/libraries/BeaconRoots.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.20;
 /// @title SSZMerkleProof
 /// @notice Library for verifying SSZ Merkle proofs
 library BeaconRoots {
-    function _get(address _beaconRoots, uint256 _timestamp) internal view returns (bytes32) {
-        (bool success, bytes memory result) = _beaconRoots.staticcall(abi.encode(_timestamp));
-        if (success && result.length > 0) {
-            return abi.decode(result, (bytes32));
-        } else {
-            return bytes32(0);
+    function _get(address _beaconRoots, uint256 _timestamp) internal view returns (bytes32 beaconRoot) {
+        assembly {
+            mstore(0, _timestamp)
+            pop(staticcall(gas(), _beaconRoots, 0, 0x20, 0, 0x20))
+            if eq(returndatasize(), 0x20) {
+                beaconRoot := mload(0)
+            }
         }
     }
 }
---
+++ contracts/test/libraries/BeaconRoots.test.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import "../mocks/BeaconRootsMock.sol";
+import "../../src/libraries/BeaconRoots.sol";
+
+
+contract BeaconRootsTest is Test {
+    BeaconRootsMock public beaconRootsMock;
+
+    function setUp() public {
+        beaconRootsMock = new BeaconRootsMock();
+        beaconRootsMock.givenTimespampReturn(0x12345, bytes32(uint256(0x12345)));
+    }
+
+    function test_get_whenRootMissing() public view {
+        bytes32 beaconRoot = BeaconRoots._get(address(beaconRootsMock), 0x1234);
+        assertEq(beaconRoot, bytes32(uint256(0)));
+    }
+
+    function test_get_whenRootNotMissing() public view {
+        bytes32 beaconRoot = BeaconRoots._get(address(beaconRootsMock), 0x12345);
+        assertEq(beaconRoot, bytes32(uint256(0x12345)));
+    }
+}
